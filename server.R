410dfd7d2b8ab23b90c44beb7e7328dc1ac208f1
Médhi Famibelle <medhi.famibelle@orange.com>
2015-02-10 10:33:51 +0100

    Added Dendrogram feature

--- server.R
+++ server.R
@@ -5,10 +5,12 @@
 #install.packages("tm")
 #install.packages("devtools")
 #library(devtools)
+# install.packages("SnowballC")
 #install_github("timjurka/sentiment")
 #install_url("http://cran.r-project.org/src/contrib/Archive/sentiment/sentiment_0.2.tar.gz")
 
 # require(sentiment)
+require(graphics)
 
 library(twitteR)
 library(shiny)
@@ -16,21 +18,31 @@ library(ROAuth)
 library(RColorBrewer)
 library(tm)
 library(wordcloud)
-# library(googleVis)
+library(googleVis)
 library(ggplot2)
 library(gridExtra)
+library(plyr)
+library(igraph)
+library(stringr)
+library(SnowballC)
 
+# load functions 
 source("sentiment/R/classify_emotion.R")
 source("sentiment/R/classify_polarity.R")
 source("sentiment/R/create_matrix.R")
+source("who-retweet.R") 
+source("cleant.txt.R")
 
+# get the world main cities and and countries WOIED (Where On Earth IDentifier)
 CountryWoeid <- fromJSON(txt = "WOEID.json", flatten =TRUE)
 TownList <-    CountryWoeid[CountryWoeid$placeType.name == "Town"    ,  ]
 CountryList <- CountryWoeid[CountryWoeid$placeType.name == "Country" ,  ]
 
+# load all the needed twitter authentication 
 load("twitter.authentication")
 registerTwitterOAuth(twitCred)
 
+# Shiny main program
 shinyServer(
     function(input, output, session) {
         r_stats <- reactive({
@@ -41,7 +53,7 @@ shinyServer(
                                             lang = input$lang,
                                             cainfo = "cacert.pem")                
             
-            #Clean the data into a neat dataframe
+            #Transform the list into a neat dataframe
             do.call("rbind", lapply(QueryResult, as.data.frame))            
         })
         
@@ -53,7 +65,8 @@ shinyServer(
         output$oid2         <- renderPrint({input$id2})
         output$odate        <- renderPrint({input$daterange})
         
-        output$sentiment <- renderPlot({
+        
+        output$sentiment <- renderGvis({
             withProgress(message = 'Calculation in progress',
                          detail = 'This may take a while...', value = 0, {
                             TweetSentiments <- r_stats()
@@ -74,25 +87,93 @@ shinyServer(
                 "isRetweet", "retweeted", "longitude", "latitude", 
                 "emotion", "polarity"
             )
+            Polarity <- count(TweetSentiments$polarity)
+            Emotion  <- count(TweetSentiments$emotion)
+            PiePolarity <- gvisPieChart(Polarity,
+                                        options=list(
+                                            title='Tweets Polarity'
+                                            )
+                                        )
+            PieEmotion  <- gvisPieChart(Emotion,
+                                        options=list(
+                                            title='Tweets Emotion'
+                                        )
+            )
+            PolarityPercentage <- Polarity
+            PolarityPercentage$freq <- round(PolarityPercentage$freq/input$n_Tweets*100,2)
+            GaugePolarityNegative <- gvisGauge(
+                PolarityPercentage[1,], labelvar = "x",
+                options=list(
+                    #                     animation = "{startup: TRUE, duration : 400 }",
+                    #                     NumberFormat = "{pattern: \"#'%'\"}",
+                    min=0, 
+                    max=100, 
+                    redFrom=75, 
+                    redTo=100
+                )
+            )
             
-            EmotionPlot <- ggplot(TweetSentiments, aes(x=emotion))
-            EmotionPlot <- EmotionPlot + geom_bar(aes(y=..count.., fill=emotion))
-            PolarityPlot <- ggplot(TweetSentiments, aes(x=polarity))
-            PolarityPlot <- PolarityPlot + geom_bar(aes(y=..count.., fill=polarity))
-            Sentiment <- grid.arrange(PolarityPlot, EmotionPlot, ncol = 2)
-            return(Sentiment)
+            Gauges <- gvisMerge(gvisMerge(PiePolarity, PieEmotion, horizontal=FALSE),
+                                GaugePolarityNegative, horizontal=TRUE, tableOptions="cellspacing=5")
+            return(Gauges)
         })
-        
+
         output$TrendingTopics <- renderDataTable({
             TT <- getTrends(TownList[TownList$name == input$town, "woeid"], cainfo = "cacert.pem")
             TrendingTopics <- as.data.frame(TT$name)
             names(TrendingTopics) <- paste("Trending in ", input$town, sep="")
             return(TrendingTopics)            
         },
-        options=list(
+            options=list(
                      lengthChange = FALSE    # show/hide records per page dropdown
+            )
         )
-        )
+        
+        output$WhoRT <- renderPlot({
+            withProgress(message = 'Collecting tweets in progress',
+                         detail = 'This may take a while...', value = 0, {
+                             Who_RT_the_Tweet(Tweet = input$TwitterQuery, no_of_tweets = input$n_Tweets, lang = input$lang)
+                         })
+            })
+
+        
+        output$dendrogram <- renderPlot({
+            withProgress(message = 'Collecting tweets in progress',
+                         detail = 'This may take a while...', value = 0, {
+                             VectorTweet <- as.vector(r_stats()[,"text"])
+                         })
+            
+            withProgress(message = 'Processing Corpus and Dendrogram',
+                         detail = 'This may take a while...', value = 0, {
+                             VectorTweet <- clean.text(VectorTweet)
+                             TweetCorpus <- Corpus(VectorSource(VectorTweet))
+#                              TweetCorpus <- tm_map(TweetCorpus,
+#                                                 content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')),
+#                                                 mc.cores=1)
+                             TweetCorpus <- tm_map(TweetCorpus, tolower)
+                            TweetCorpus <- tm_map(TweetCorpus, function(x) removeWords(x,c("http://t.co/*", "https://t.co/*", "RT", stopwords(input$lang))))
+
+                             TweetCorpus <- tm_map(TweetCorpus, removePunctuation)
+                             TweetCorpus <- tm_map(TweetCorpus, removeNumbers)
+                             TweetCorpus <- tm_map(TweetCorpus, PlainTextDocument)
+                             # keep a copy of corpus to use later as a dictionary for stem completion
+                             myCorpus <- TweetCorpus
+                             myCorpusCopy <- myCorpus
+                             # stem words
+                             myCorpus <- tm_map(myCorpus, stemDocument)
+                             # stem completion
+#                              myCorpus <- tm_map(myCorpus, stemCompletion, dictionary = myCorpusCopy)
+                             tdm <- TermDocumentMatrix(myCorpus, control = list(wordLengths = c(1, Inf)))
+                             # remove sparse terms
+                             tdm2 <- removeSparseTerms(tdm, sparse = 0.95)
+                             m2 <- as.matrix(tdm2)
+                             # cluster terms
+                             distMatrix <- dist(scale(m2))
+                             fit <- hclust(distMatrix, method = "ward.D")
+                             plot(fit)
+                             rect.hclust(fit, k = 6) # cut tree into 6 clusters
+                         })
+        })        
         
         output$plot <- renderPlot({
             withProgress(message = 'Collecting tweets in progress',
@@ -103,11 +184,11 @@ shinyServer(
             withProgress(message = 'Processing word cloud',
                          detail = 'This may take a while...', value = 0, {
                 Tweet_palette <-brewer.pal(9,"Set1")
-                VectorTweet <- gsub("http\\w+", "", VectorTweet)
+                VectorTweet <- clean.text(VectorTweet)
                 
                 TweetCorpus <- Corpus(VectorSource(VectorTweet))
                 TweetCorpus <- tm_map(TweetCorpus, tolower)
-                TweetCorpus <- tm_map(TweetCorpus, function(x) removeWords(x,c("http://t.co/*", "https://t.co/*", stopwords(input$lang))))
+                TweetCorpus <- tm_map(TweetCorpus, function(x) removeWords(x,c("http://t.co/*", "https://t.co/*", "RT", stopwords(input$lang))))
                 TweetCorpus <- tm_map(TweetCorpus, removePunctuation)
                 TweetCorpus <- tm_map(TweetCorpus, removeNumbers)
                 TweetCorpus <- tm_map(TweetCorpus, PlainTextDocument)
@@ -118,43 +199,5 @@ shinyServer(
                           colors=Tweet_palette)
             })
         })
-        
-        #         output$mapPlot <- renderGvis({
-        #             TrendLocation <- data.frame(matrix(NA, nrow = 245, ncol = 3))
-        #             names(TrendLocation) <- c("name", "country", "woeid")
-        #             
-        #             for (i in 1:26) {
-        #                 TrendLocation[i,] <- closestTrendLocations(countryList$latitude[i], countryList$longitude[i])
-        #                 TrendLocation[i,4] <- getTrends(TrendLocation$woeid[i])$name[1]
-        #                 print(i)
-        #             }
-        #             TrendLocation <- TrendLocation[,-1]
-        #             names(TrendLocation) <- c("country", "woeid", "trending")
-        #             GeoMap <- gvisGeoMap(TrendLocation[c(1:26),], locationvar="country", hovervar="Trending")
-        #             plot(GeoMap)
-        #                 #TT[i,"woeid"] <- TrendLocation$country[i]
-        #                 #names(TT) <- c("name",  "url",   "query", "country")
-        #                 #data <- cbind(TT$country, TT$name)
-        #                 #data <- as.data.frame(data)
-        #                 #data <- cbind(data, as.data.frame(10:1))
-        #                 #data <- as.data.frame(data)
-        #                 #data <- data[,c(1,3,2)]
-        #                 #names(data) <- c("Country", "Int", "Trending")
-        #                 
-        #             
-        #             #TrendLocation <- closestTrendLocations(input$latitude, input$longitude)
-        #             #TT <- getTrends(TrendLocation$woeid)
-        #             #TT[,"woeid"] <- TrendLocation$country
-        #             #names(TT) <- c("name",  "url",   "query", "country")
-        #             #data <- cbind(TT$country, TT$name)
-        #             #data <- as.data.frame(data)
-        #             #data <- cbind(data, as.data.frame(10:1))
-        #             #data <- as.data.frame(data)
-        #             #data <- data[,c(1,3,2)]
-        #             #names(data) <- c("Country", "Int", "Trending")
-        #             #GeoMap <- gvisGeoMap(data, locationvar="Country", hovervar="Trending")
-        #             #return(GeoMap)
-        #             
-        #         })
     }
 )
--- ui.R
+++ ui.R
@@ -1,28 +1,31 @@
 library(rjson)
 library(jsonlite)
 
+# get the world main cities and and countries WOIED (Where On Earth IDentifier)
 CountryWoeid <- fromJSON(txt = "WOEID.json", flatten =TRUE)
 TownList <-    CountryWoeid[CountryWoeid$placeType.name == "Town"    ,  ]
 CountryList <- CountryWoeid[CountryWoeid$placeType.name == "Country" ,  ]
 
+# Load all the Twitter supported languages
+languages <- fromJSON(txt = "language.json", flatten =TRUE)
+
+
+#Add Google Analytics to the Shiny app
+tags$head(includeScript("google-analytics.js"))
+
 
 shinyUI(pageWithSidebar(
+    
     headerPanel("A Minimum Viable Twitter Analysis Tool"),
     sidebarPanel(
         textInput('TwitterQuery', "Text to be searched (#, @ included): ", "#JeSuisCharlie"), #labeled TwitterQuery
         #numericInput("woeid", "Where On Earth Identifiers: ", 615702, min = 1),
         numericInput('n_Tweets', 'Number of tweets to retrieve: ', 101, min = 1, max = 1500, step = 1), #labeled n_Tweets
-        radioButtons("lang","Select the language",c(
-            "English"="en",
-            "French"="fr",
-            "Spanish"="es",
-            "Deutsch"="de",
-            "Russian"="ru"
-        )
+        selectInput("lang","Select the language",
+                    choices = languages$code
         ),
         
 #         checkboxInput("checkbox", label = "Take into account GPS coordinate ?", value = FALSE),
-        
 #         sliderInput("latitude", "Latitude",   -25,  min = -90,  max = 90,  step = .5),
 #         sliderInput("longitude", "Longitude", 135, min = -180, max = 180, step = .5),
         
@@ -40,12 +43,12 @@ shinyUI(pageWithSidebar(
     mainPanel(
         tabsetPanel(
             tabPanel("Words Cloud", plotOutput("plot")),
-            tabPanel("Sentiment Analysis", plotOutput("sentiment")), #Soon when sentiment install_github("timjurka/sentiment") problem is fixed
+            tabPanel("Sentiment Analysis", htmlOutput("sentiment")),
             tabPanel("Tweets", dataTableOutput("TwitterQuery")),
+            tabPanel("Who has RT the tweets", plotOutput("WhoRT")),
+            tabPanel("Cluster Dendrogram", plotOutput("dendrogram")),
             tabPanel("Trending Topics by major cities", dataTableOutput("TrendingTopics"))
             #tabPanel("Trending Map", htmlOutput("mapPlot")) #soon             
         )
-        
-        
     )
 ))
